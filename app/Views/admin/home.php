26fa44345e361688eb6a027ffab4f82a4b78dc5f
Abel Ananta <abelananta9@gmail.com>
2020-11-22 14:11:46 +0700

    modif card info

--- app/Views/admin/home.php
+++ app/Views/admin/home.php
@@ -2,43 +2,46 @@
 
 <?= $this->section('content'); ?>
 
-<div class="row row-cols-1 row-cols-md-2">
-    <div class="col mb-4">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<center>
+    <div class="col d-flex justify-content-center">
+        <div class="card-deck">
+            <div class="card text-white bg-primary mb-3 mr-4" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Pembayaran Suhat</h5>
+                    <p class="card-text">Berisi Data Pembayaran Suhat Dengan Status On-Going.</p>
+                </div>
+                <a href="/admin/suhat">
+                    <div class="card-footer">Klik Disini</div>
+                </a>
+            </div>
+            <div class="card text-white bg-success mb-3 mr-4" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Riwayat Suhat</h5>
+                    <p class="card-text">Berisi Data Riwayat Seluruh Pembayaran Suhat Yang Telah Selesai.</p>
+                </div>
+                <a href="/admin/riwayat">
+                    <div class="card-footer">Klik Disini</div>
+                </a>
+            </div>
+            <div class="card text-white bg-info mb-3 mr-4" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Pembayaran Candi</h5>
+                    <p class="card-text">Berisi Data Pembayaran Candi Dengan Status On-Going.</p>
+                </div>
+                <a href="/admin/candi">
+                    <div class="card-footer">Klik Disini</div>
+                </a>
+            </div>
+            <div class="card text-white bg-warning mb-3 mr-4" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Riwayat Candi</h5>
+                    <p class="card-text">Berisi Data Riwayat Seluruh Pembayaran Candi Yang Telah Selesai.</p>
+                </div>
+                <a href="#">
+                    <div class="card-footer">Klik Disini</div>
+                </a>
             </div>
         </div>
     </div>
-    <div class="col mb-4">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-        </div>
-    </div>
-</div>
-
+</center>
 <?= $this->endSection(); ?>
