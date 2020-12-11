b0f845e6cdd8fbb7a06c9b19c0b57e651ff04137
Abel Ananta <abelananta9@gmail.com>
2020-12-11 19:33:29 +0700

    halaman print

--- app/Views/admin/print.php
+++ app/Views/admin/print.php
@@ -8,6 +8,12 @@
 </head>
 
 <body>
+    <center>
+        <h1>Data Booking Homestay Aktif</h1>
+        <?= date("Y-m-d") ?><br>
+    </center>
+    <br>
+    <br>
     <table class="table table-bordered">
         <thead>
             <tr>
@@ -15,8 +21,11 @@
                 <th scope="col">ID Bayar</th>
                 <th scope="col">No Kamar</th>
                 <th scope="col">Nama</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">No. Telp</th>
                 <th scope="col">Check-In</th>
                 <th scope="col">Check-Out</th>
+                <th scope="col">Pembayaran</th>
             </tr>
         </thead>
         <tbody>
@@ -27,13 +36,15 @@
                     <td><?php echo $byr['id_bayar']; ?></td>
                     <td><?php echo $byr['no_kamar']; ?></td>
                     <td><?php echo $byr['nama']; ?></td>
+                    <td><?php echo $byr['alamat']; ?></td>
+                    <td><?php echo $byr['no_telp']; ?></td>
                     <td><?php echo $byr['check_in']; ?></td>
                     <td><?php echo $byr['check_out']; ?></td>
+                    <td><?php echo $byr['pembayaran']; ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
-
     <script type="text/javascript">
         window.print();
     </script>
